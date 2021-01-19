82948655f97ba57ead12d90cd0fc30c7ea10f0b7
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro.local>
2021-01-19 10:16:11 -0600

    update

--- code/eddyflux.R
+++ code/eddyflux.R
@@ -26,3 +26,29 @@ ggplot(day)+
   xlab("")+
   ylab("CH4 Flux (µmol m-2 d-1)")+
   theme_bw()
+
+# Read in data for TB
+dataTB = read_csv('data/eddypro_TB_data.csv')
+fluxTB<- dataTB%>%
+  filter(ch4_flux > -9999)
+fluxTB$date<- as.Date(fluxTB$date, format = "%m/%d/%y")
+
+ggplot(fluxTB)+
+  geom_line(aes(x = date, y = ch4_flux))+
+  geom_vline(xintercept = as.numeric(as.Date("2020-04-28")),  linetype = "dashed", color = "blue")+
+  xlab("")+
+  ylab("CH4 Flux (µmol m-2 s-1)")+
+  theme_bw()
+
+#mgC/m2/day
+dayTB<- fluxTB %>%
+  group_by(date) %>%
+  summarize(mean_flux = mean(ch4_flux))
+
+ggplot(dayTB)+
+  geom_line(aes(x = date, y = mean_flux))+
+  geom_vline(xintercept = as.numeric(as.Date("2020-04-28")),  linetype = "dashed", color = "blue")+
+  xlab("")+
+  ylab("CH4 Flux (µmol m-2 d-1)")+
+  theme_bw()
+
