a2ea91c7f9b882abc3679806e345d5179bda768e
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro.local>
2021-01-22 13:43:23 -0600

    update

--- code/GCAnalysis.R
+++ code/GCAnalysis.R
@@ -5,14 +5,13 @@ library(neonDissGas)
 
 # Read in data 
 df = read_csv('data/2021Run1/rawdata.csv')  %>% 
+  filter(batch_id == 'SSB')%>%
   mutate(sample_id = str_replace_all(sample_id, pattern = "2020_01_", replacement = "2020-01-")) %>%  #because you switch from underscores to dashes
-  separate(col = sample_id, into = c('lake','date','depth', 'replicate'), sep = "_", remove = FALSE) %>% 
-  mutate(date = as.Date(date)) %>% 
-  mutate(lakedepth = as.numeric(depth)) %>% 
-  filter(lake == 'TB' | lake == 'SSB')
+  separate(col = sample_id, into = c('lake','date','depth','replicate'), sep = "_", remove = FALSE) %>% 
+  mutate(date = as.Date(date)) 
 
 air_samples <- df %>% filter(grepl("Air",depth)) %>% 
-  select(-analysis_date,-batch_id,-sample_id,-depth,-lakedepth) %>% 
+  select(-analysis_date,-batch_id,-sample_id,-depth) %>% 
   group_by(lake,date) %>% 
   summarize_all(list(median = median)) %>% 
   select(-CO2_TCD_median,-CH4_TCD_median) %>%  #levels are all to low to use TCD
@@ -44,19 +43,4 @@ tidy.dat.out2021 <- def.calc.sdg.conc(as.data.frame(dat)) %>%
 
 write.table(tidy.dat.out, 'data/tidy.dat.out2021.csv', sep="\t")
 
-
-ggplot(data = dat.out,aes(x=date,y=value*1000000,color=as.factor(lakedepth))) + geom_point(alpha=0.5) + facet_wrap(vars(lake,parameter)) + labs(color="sampledepth",y="dissolved gas (umol/L)") + scale_y_log10()
-
-temperature.prof<-datnew%>%
-  group_by(date, lake, lakedepth)%>%
-  summarize(Mean = mean(waterTemp))
-
-ggplot(dplyr::filter(temperature.prof, lake == 'TB' | lake == 'SSB'))+
-  geom_point(aes(x = Mean, y = lakedepth))+
-  geom_line(aes(x = Mean, y = lakedepth, factor = date, color = date))+
-  facet_wrap(~lake) +
-  scale_y_reverse()+
-  theme_bw()
-
-ggsave("Bog_Gas.png")
 
