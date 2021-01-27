512cb99943bd0313c514d16171ecf583ee6da109
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro.local>
2021-01-27 13:28:34 -0600

    update

--- code/AllGasProfiles.R
+++ code/AllGasProfiles.R
@@ -3,66 +3,78 @@ library(devtools)
 library(lubridate)
 library(neonDissGas)
 library(gridExtra)
+library(patchwork)
 
 ## Load the data from Github
 # Read in data 
-prac = read_csv('data/tidy.dat.out2021.csv')
-prac$date = as.Date(prac$date, format =  "%m/%d/%y")
+prac2021 = read_csv('data/GC2021/tidy.dat.out2021.csv')
+prac2020 = read_csv('data/tidy.dat.out.csv')
+gas<- rbind(prac2021, prac2020)
+gas$date = as.Date(gas$date, format =  "%m/%d/%y")
+gas$doy = yday(gas$date)
 
-gd <- prac %>% 
-  group_by(lake, date, depth) %>% 
+gd <- gas %>% 
+  group_by(lake, date, depth, doy) %>% 
   summarise(
     CO2 = mean(dissolvedCO2),
     CH4 = mean(dissolvedCH4)
-  )
+  )%>%
+  mutate(icecovered = ifelse(month(date)%in% 1:4,"yes",
+                             "no"))
+  
 
 # CO2
-ggplot(dplyr::filter(gd, lake == 'TB'| lake == 'SSB')) + 
-  geom_point(aes(x = CO2*1000000, y = depth, color = date),size = 3) +
-  geom_path(aes(x = CO2*1000000, y = depth, color = date)) +
+CO2<-ggplot(dplyr::filter(gd, lake == 'TB'| lake == 'SSB')) + 
+  geom_point(aes(x = CO2*1000000, y = depth, color = doy, shape = icecovered),size = 3) +
+  geom_path(aes(x = CO2*1000000, y = depth, group = date, color = doy)) +
   facet_wrap(~lake) +
   scale_y_reverse(name = "Depth (m)") +
   scale_x_continuous(name = "dissolved CO2 gas (umol/L)", limits = c(0, 2000))+
+  scale_colour_viridis_c()+
   theme_bw()+
   theme(legend.position = "none")
 
 # CH4
-ggplot(dplyr::filter(gd, lake == 'SSB')) + 
-  geom_point(aes(x = CH4*1000000, y = depth, color = date),size = 3) +
-  geom_path(aes(x = CH4*1000000, y = depth, color = date)) +
+CH4<-ggplot(dplyr::filter(gd, lake == 'TB'| lake == 'SSB')) + 
+  geom_point(aes(x = CH4*1000000, y = depth, color = doy, shape = icecovered),size = 3) +
+  geom_path(aes(x = CH4*1000000, y = depth, group = date, color = doy)) +
+  facet_wrap(~lake) +
   scale_y_reverse(name = "Depth (m)") +
   scale_x_continuous(name = "dissolved CH4 gas (umol/L)")+
+  scale_colour_viridis_c(name = "Day of Year")+
+  scale_shape_discrete(name = "Ice Covered")+
   theme_bw()+
-  theme(legend.position = "none")
+  theme()
+
+# Read in data 
+tempdo = read_csv('data/ChemTempDO/tempdo.csv')
+tempdo$date = as.Date(tempdo$Date, format =  "%m/%d/%y")
+tempdo<- tempdo%>%
+  mutate(icecovered = ifelse(month(date)%in% 1:4,"yes",
+                             "no"))
+
 
 #Facet_Wrap by Date for Temperature and DO for Trout Bog
-Temp<-ggplot(dplyr::filter(dat, Bog == 'TB' | Bog == 'SSB')) + 
-  geom_point(aes(x = waterTemp, y = Depth)) +
-  geom_path(aes(x = waterTemp, y = Depth))+
-  facet_wrap(~date) +
+Temp<-ggplot(dplyr::filter(tempdo, Bog == 'TB' | Bog == 'SSB')) + 
+  geom_point(aes(x = waterTemp, y = Depth, color = date, shape = icecovered), size = 1.5) +
+  geom_path(aes(x = waterTemp, y = Depth, color = date, group = date))+
+  facet_wrap(~Bog) +
   scale_y_reverse(name = "Depth (m)") +
   scale_x_continuous(name = "Temperature (C)")+
-  theme_bw()+
-  theme(legend.position = "none")
-#Facet_Wrap by Date for Temperature and DO
-DO<-ggplot(dplyr::filter(dat, Bog == 'TB' | Bog == 'SSB')) + 
-  geom_point(aes(x = waterTemp, y = Depth)) +
-  geom_path(aes(x = waterTemp, y = Depth))+
-  facet_wrap(~date) +
-  scale_y_reverse(name = "Depth (m)") +
-  scale_x_continuous(name = "Temperature (C)")+
+  scale_colour_viridis_c()+
   theme_bw()+
   theme(legend.position = "none")
 
-O2<-ggplot(dplyr::filter(dat, Bog == 'TB' | Bog == 'SSB')) + 
-  geom_point(aes(x = DO, y = Depth, color = col, size = 1.5)) +
-  geom_path(aes(x = DO, y = Depth, color = col))+
+O2<-ggplot(dplyr::filter(tempdo, Bog == 'TB' | Bog == 'SSB')) + 
+  geom_point(aes(x = DO, y = Depth, color = month(date), shape = icecovered), size = 1.5) +
+  geom_path(aes(x = DO, y = Depth, color = month(date), group = date))+
   facet_wrap(~Bog) +
   scale_y_reverse(name = "Depth (m)") +
   scale_x_continuous(name = "Dissolved Oxygen (mg/L)")+
+  scale_colour_viridis_c()+
   theme_bw()+
   theme(legend.position = "none")
 
-grid.arrange(CO2,CH4,Temp,O2, ncol = 2, nrow =2)
+(CO2 + CH4 + Temp + O2) + plot_layout(guides = "collect", ncol = 2)
 
-ggsave("figure.png", width = 6.5, height = 6, units = 'in', figure)
+hggsave("figure.png", width = 6.5, height = 6, units = 'in', figure)
