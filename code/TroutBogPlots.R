63fd60a5c4a3b7b07f9e5ca02f4efbf17c1d988f
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro.local>
2021-03-29 17:22:28 -0500

    update

--- code/TroutBogPlots.R
+++ code/TroutBogPlots.R
@@ -109,53 +109,56 @@ gas<- (CH4 + CO2) + plot_layout(guides = "collect", ncol = 1)
 ggsave("figures/HeatMap_TB.png", width = 10, height = 6, units = 'in', gas)
 
 #Heat Map Temperature
-buoy.2020 = read_csv('data/Buoy/temp.buoy2020.csv')
-buoy.2020$Date.GMT <- as.Date(buoy.2020$Date.GMT)
-buoy.2020<- buoy.2020%>%
-  mutate(sampledate = as.POSIXct(Date.GMT, format="%Y-%m-%dT%H:%M"))
+tempdo = read_csv('data/ChemTempDO/TBtempdo.csv')
+tempdo$date = as.Date(tempdo$date, format =  "%m/%d/%y")
+tempdoTB <- tempdo%>%
+  mutate(sampledate = date)%>%
+  select(sampledate, depth, waterTemp)
+
+tempAnnual <- tempdoTB %>%
+  filter(date <= as.POSIXct("2021-01-01"))
 
 interpDataTemp <- function(observationDF, date, maxdepth) {
   a = observationDF %>% filter(sampledate == date)
-  if (sum(!is.na(a$Temp.C)) == 0) {
+  if (sum(!is.na(a$waterTemp)) == 0) {
     print('nothing')
     return(NULL)
   }
   
-  b = a %>% filter(!is.na(Temp.C))
+  b = a %>% filter(!is.na(waterTemp))
   if (max(b$depth) < (maxdepth/2)) {
     print('too shallow')
     return(NULL)
   }
   
-  yout = approx(x = a$depth, y = a$Temp.C, xout = c(0:maxdepth), rule = 2)
+  yout = approx(x = a$depth, y = a$waterTemp, xout = c(0:maxdepth), rule = 2)
   return(yout$y)
 }
 
 maxdepth = 7 # Should be depth of lowest sample, not necessarily depth of lake 
-usedates = buoy.2020 %>%
-  dplyr::distinct(sampledate) 
+usedatesTemp = tempdoTB %>%
+  dplyr::distinct(sampledate)
 
-f <- lapply(X = usedates$sampledate, FUN = interpData, observationDF = buoy.2020,
-            maxdepth = maxdepth)
-
-f = as.data.frame(do.call(cbind, f))
-names(f) = usedates$sampledate
+fTBTemp <- lapply(X = usedatesTemp$sampledate, FUN = interpDataTemp, observationDF = tempdoTB,
+                 maxdepth = maxdepth)
+fTBTemp = as.data.frame(do.call(cbind, fTBTemp))
+names(fTBTemp) = usedatesTB$sampledate
 
 # Bind list into dataframe
-f2 = bind_cols(depth = 0:maxdepth,f) %>%
+f2TBTemp = bind_cols(depth = 0:maxdepth,fTBTemp) %>%
   pivot_longer(-1, names_to = 'sampledate', values_to = 'var') %>%
   arrange(sampledate,depth) %>%
   mutate(sampledate = as.Date(sampledate))
 
 # Heat map 
-theme_set(theme_bw())
-tempheat<-ggplot(f2) +
-  guides(fill = guide_colorsteps(barheight = unit(4, "cm")), name = "Temp (degC)") +
+ggplot(f2TBTemp) +
+  guides(fill = guide_colorsteps(barheight = unit(4, "cm"))) +
   geom_contour_filled(aes(x = sampledate, y = depth, z = var)) +
+  geom_point(data = tempdoTB, aes(x = sampledate, y = depth), size = 0.25, color = 'white') +
   scale_y_reverse()  +
   scale_color_viridis_c(name = var) +
   ylab('Depth (m)') + xlab('') +
-  #xlim(as.Date(paste0(2018,'-01-01')), as.Date(paste0(2018,'-12-31'))) +
-  theme_bw(base_size = 8)+
-  scale_x_date(breaks = "2 month", minor_breaks = "1 month", labels=date_format("%b %y"),
-               limits = c(as.Date(paste0(2019,'-12-15')), as.Date(paste0(2020,'-10-26'))))
+  labs(fill = ((expression(paste("Temperature (C)")))))+
+  theme_bw(base_size = 8) +
+  scale_x_date(breaks = "2 month", minor_breaks = "1 month", labels=date_format("%b, %Y"),
+               limits = c(as.Date(paste0(2020,'-01-01')), as.Date(paste0(2021,'-1-15'))))
